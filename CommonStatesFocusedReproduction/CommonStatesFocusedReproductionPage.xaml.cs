16a97d93626d788b1866e2543d955c03f3d85e7b
P3PPP <ticktackmob@gmail.com>
2018-03-22 01:35:12 +0900

    create projects.

---
+++ CommonStatesFocusedReproduction/CommonStatesFocusedReproductionPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace CommonStatesFocusedReproduction
+{
+	[Xamarin.Forms.Xaml.XamlCompilation(Xamarin.Forms.Xaml.XamlCompilationOptions.Compile)]
+	public partial class CommonStatesFocusedReproductionPage : ContentPage
+	{
+		public CommonStatesFocusedReproductionPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
